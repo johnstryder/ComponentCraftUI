d5b1cfe19fd7b648263e50170edcd7250fe58161
John Stryder <curtainknit@gmail.com>
2025-01-14 11:57:09 -0500

    yoo

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Get the PocketBase URL from environment variables
-const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL || 'http://localhost:8090';
+const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL;
 
 // Create and export the PocketBase client instance
-export const pb = new PocketBase(pocketbaseUrl); 
+export const pb = new PocketBase(pocketbaseUrl);
